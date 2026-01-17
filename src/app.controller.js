193014b5c1bf1234bd67f87d989500d552fd49b0
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-01-17 22:09:55 +0200

    fix env config

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,4 +1,4 @@
-import { config } from "../config.js";
+import { config } from "./Config/config.js";
 import express from "express";
 
 import { userRouter, authRouter, blogRouter } from "./Modules/index.js";
