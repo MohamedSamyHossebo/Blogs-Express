9f78ea7e7d73cff8ff5556a23045501ad73ed215
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-01-17 22:15:52 +0200

    remove unused code

--- src/Modules/user/user.controller.js
+++ src/Modules/user/user.controller.js
@@ -7,21 +7,6 @@ router.get("/", (req, res) => {
     userService.getAllUsers(res);
 });
 
-// router.get("/users/:id", (req, res) => {
-//     const id = req.params.id;
-//     try {
-//         connection.execute(`SELECT * FROM users WHERE id = ?`, [id], (err, results) => {
-//             if (err) {
-//                 return res.status(500).json({ message: err.message });
-//             } else {
-//                 return res.status(200).json({ message: "user found", results });
-//             }
-//         });
-//     } catch (err) {
-//         console.log(err);
-//     }
-// });
-
 router.get("/profile/:userId", (req, res) => {
     userService.getUserById(req.params.userId, res);
 });
